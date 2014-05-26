4000c9feff9b2caa90f042fd896f323449211f52
Rajeef <rajeefmk@gmail.com>
2014-05-26 12:21:47 +0530

    programbydoing java

---
+++ programbydoing_java/GoodFirstProgram.java
@@ -0,0 +1,8 @@
+public class GoodFirstProgram{
+	public static void main( String[] args){
+		System.out.println("I love coding");
+		System.out.println("I love java");
+		System.out.println("I love android");
+		System.out.println(" I really \"meant\" when I said I love coding, java and android");
+	}
+}
---
+++ programbydoing_java/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials{
+	public static void main(String[] args){
+	
+		System.out.println("m   m  k   k");
+		System.out.println("mm mm  k  k");
+		System.out.println("mm mm  k k");
+		System.out.println("m m m  kk");
+		System.out.println("m   m  k k");
+		System.out.println("m   m  k  k");
+		System.out.println("m   m  k   k");
+	}
+}
